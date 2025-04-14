40845f81437b23f7b6ecee79598e3195c734429b
SrinivasReddyMunugala <37141421+SrinivasReddyMunugala@users.noreply.github.com>
2025-04-14 12:29:16 -0500

    Update WeatherForecastController.cs

--- Srini.Template.Sample/Controllers/WeatherForecastController.cs
+++ Srini.Template.Sample/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace Srini.Template.Sample.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            var test = "Srinivas";
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
